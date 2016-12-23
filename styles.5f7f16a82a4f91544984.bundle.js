d77b4fe6acdf7a3dc1488c78543ef9e2fd6e2bd3
Eudes Petonnet-Vincent <petonnet.eudes@gmail.com>
2016-12-23 01:48:35 +0100

    Live test of the final website version

--- styles.5f7f16a82a4f91544984.bundle.js
+++ styles.5f7f16a82a4f91544984.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([6,7],{356:function(n,o){},357:function(n,o){},358:function(n,o){},653:function(n,o,c){c(357),c(356),n.exports=c(358)}},[653]);
+webpackJsonp([6,7],{1033:function(n,o,c){c(457),c(456),n.exports=c(458)},456:function(n,o){},457:function(n,o){},458:function(n,o){}},[1033]);
 //# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
